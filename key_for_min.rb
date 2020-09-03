3e6c51c4ba9ef75b26e88f8b97ccab4ae9f5c9b5
Jake Peterson <jpeterson0514@gmail.com>
2020-09-03 23:00:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   low_price = 0 
   low_name = nil 
   name_hash.each do |name, price|
-    if low_price == 0 && price 
-    
-
+    if low_price == 0 && price < low_price
+      low_price = price 
+      low_name = name 
+    end 
+  end 
+  low_name
 end
